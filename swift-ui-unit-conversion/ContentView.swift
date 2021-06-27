36aa55ad51a097a4a93dddfd480162655a0ac0c0
gmahima <mahimagangavarapu@gmail.com>
2021-06-27 23:52:54 +0530

    Initial Commit

---
+++ swift-ui-unit-conversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-ui-unit-conversion
+//
+//  Created by G H Mahimaanvita on 27/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        get {
+            return Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/) // a primitive view
+        }
+    } //computed property conforming to View
+} //struct conforming to View
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-ui-unit-conversion/swift_ui_unit_conversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_unit_conversionApp.swift
+//  swift-ui-unit-conversion
+//
+//  Created by G H Mahimaanvita on 27/06/21.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_unit_conversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
